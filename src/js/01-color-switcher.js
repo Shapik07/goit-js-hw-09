0edefad5c59fa9175a409fcccc349c2aa36a3a5a
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-09-09 17:31:49 +0300

    complete task #1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+startBtn.addEventListener('click', changeColor);
+stopBtn.addEventListener('click', stopChangeColor);
+
+let colorId = null;
+
+function changeColor() {
+        colorId = setInterval(() => {
+            document.body.style.backgroundColor = getRandomHexColor();
+            startBtn.setAttribute('disabled', true);
+            stopBtn.removeAttribute('disabled');
+        }, 1000);
+};
+
+function stopChangeColor() {
+    clearInterval(colorId);
+    stopBtn.setAttribute('disabled', true);
+    startBtn.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
