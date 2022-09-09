aebb7fc1c81a9d27efbe66bbb81ce072d2cda41a
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-09-09 20:47:35 +0300

    Update

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,16 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+
+const startBtn = document.querySelector('[data-start]');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr('#datetime-picker', options);
